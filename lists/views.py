bc26b52bf98c3eb330868a6159d0ed60c3860151
tfung92 <tfung92@users.noreply.github.com>
2018-10-21 21:39:16 -0400

    updated lists

--- lists/views.py
+++ lists/views.py
@@ -12,6 +12,7 @@ def view_list(request):
     return render(request, 'list.html', {'items': items})
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
